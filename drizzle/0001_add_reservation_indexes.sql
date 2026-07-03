ba9a380987d0313e6416e1d8f0eb622756e42850
mariomatrix <mario@imagomatrix.hr>
2026-07-03 14:43:54 +0200

    chore: ispravna migracija za bazu

--- drizzle/0001_add_reservation_indexes.sql
+++
@@ -1,7 +0,0 @@
-CREATE INDEX IF NOT EXISTS "res_scheduled_start_idx" ON "reservations" ("scheduled_start");
-CREATE INDEX IF NOT EXISTS "res_scheduled_end_idx" ON "reservations" ("scheduled_end");
-CREATE INDEX IF NOT EXISTS "res_status_idx" ON "reservations" ("status");
-CREATE INDEX IF NOT EXISTS "res_user_id_idx" ON "reservations" ("user_id");
-CREATE INDEX IF NOT EXISTS "res_crane_id_idx" ON "reservations" ("crane_id");
-CREATE INDEX IF NOT EXISTS "res_requested_date_idx" ON "reservations" ("requested_date");
-CREATE INDEX IF NOT EXISTS "res_is_maintenance_idx" ON "reservations" ("is_maintenance");
